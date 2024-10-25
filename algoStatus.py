d69a989d7fe59a2d3f443f3deada8e7fd969a76c
Rahul Kumar Mandal <rahulm@evincedev.com>
2024-10-25 10:49:57 +0530

    algo update

--- algoStatus.py
+++ algoStatus.py
@@ -5,8 +5,10 @@ payload = ''
 headers = {
   'accept': 'application/json, text/plain, */*',
   'accept-language': 'en-US,en;q=0.9',
-  'authorization': 'csrftoken=gDBp1Ez90Cra7zrR7Wxrw0cFmdhuMSy0lzhAm64DZE5OpW8rteDW6NZXFPCAlX67;csrfmiddlewaretoken=gDBp1Ez90Cra7zrR7Wxrw0cFmdhuMSy0lzhAm64DZE5OpW8rteDW6NZXFPCAlX67;sessionid=G6Ogzl5uXyeWPojYSrTjRgQKbmOPjjrx',
+  'authorization': 'csrftoken=isKIO0LzgCRbLEjxDwmLXqOgi5gUx0L2n9Z3heTQNOTb4gjm0F18E2oi3Tr2MM16;csrfmiddlewaretoken=isKIO0LzgCRbLEjxDwmLXqOgi5gUx0L2n9Z3heTQNOTb4gjm0F18E2oi3Tr2MM16;sessionid=TGnxUWU2fGqKaaaqEcYtbPdHE2G9YJbQ',
+  'cache-control': 'no-cache',
   'origin': 'https://www.streak.tech',
+  'pragma': 'no-cache',
   'priority': 'u=1, i',
   'referer': 'https://www.streak.tech/',
   'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
@@ -16,7 +18,7 @@ headers = {
   'sec-fetch-mode': 'cors',
   'sec-fetch-site': 'same-site',
   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-  'Cookie': 'streak_capp=MTcyOTM0ODQ2OXxRd3dBUURveE9tUnFZVzVuYnk1amIyNTBjbWxpTG5ObGMzTnBiMjV6TG1OaFkyaGxaalo2TkhsR2NrTmpSSFpLT1U5NFZWTjRla05KYUVrM2NWbG5hRmMwYnpNPXw7GoW__H0H7J-3MgoLxrqax9mlGRUQo-FO99KvJ4zeAQ=='
+  'Cookie': 'streak_capp=MTcyODg4NDE0NHxRd3dBUURveE9tUnFZVzVuYnk1amIyNTBjbWxpTG5ObGMzTnBiMjV6TG1OaFkyaGxiVzl5Tm1abloxUmpjM1J6YjFZMGFqRjZjekpMZGpGTE5uVnRORUZpUmpnPXzWQE76Ald2ciRFFIPsizNsPae-sPzDFHux6DMmGDBVUw=='
 }
 conn.request("GET", "/fetch_order_log/?deployment_uuid=c2d0656b-bb7d-4d10-b443-abcd56f9c113", payload, headers)
 res = conn.getresponse()
